7d39cdc4f6c4b6c6010b29bc55be6c9bc8a1a110
puppalakshitha <99403589+puppalakshitha@users.noreply.github.com>
2022-02-17 21:24:54 +0530

    Update experiment 11.asm

--- C++ lab/Experiment-1/experiment 11.asm
+++ C++ lab/Experiment-1/experiment 11.asm
@@ -1,1 +1,28 @@
-
+#include<iostream> using namespace std; class Grand_parent{
+public:
+int a; Grand_parent(){
+a = 10;
+}
+void display1(){
+cout<<"a = "<<a<<endl;
+}
+};
+class parent:public Grand_parent{ public:
+int b; parent(){
+b = 20;
+}
+void display2(){
+cout<<"b = "<<b<<endl;
+}
+};
+class child:public parent{ public:
+int c; child(){
+c = 30;
+}
+void display3(){
+cout<<"c = "<<c<<endl;
+}
+};
+int main(){
+child obj; obj.display1(); obj.display2(); obj.display3(); return 0;
+}
