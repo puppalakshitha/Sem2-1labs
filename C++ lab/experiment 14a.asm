9d4ae27e8748222d238a40373300d39a716039d5
puppalakshitha <99403589+puppalakshitha@users.noreply.github.com>
2022-02-17 21:38:48 +0530

    Create experiment 14a.asm

---
+++ C++ lab/experiment 14a.asm
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+char x;
+cout<<”Enter the character:”; cin.get(x);
+cout << “The given Character is:” cout.put(x);
+cout.write("Welcome to Hyderabad", 5); return 0;
+}
